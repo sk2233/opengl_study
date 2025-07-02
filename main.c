98158d42cdc853d1d1aad1cec509214b0bed8c87
sky <2171783438@qq.com>
2025-07-02 16:35:10 +0800

    测试pbr

--- main.c
+++ main.c
@@ -25,44 +25,24 @@ int main(){
     glfwSetKeyCallback(window,key_callback);
     glfwSetInputMode(window,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
 
-    uint32_t baseShader = open_shader("/Users/sky/Documents/c/open_gl/shader/bloom.vert", "/Users/sky/Documents/c/open_gl/shader/bloom.frag",NULL);
-    uint32_t lightShader = open_shader("/Users/sky/Documents/c/open_gl/shader/bloom.vert", "/Users/sky/Documents/c/open_gl/shader/light_box.frag",NULL);
-    uint32_t blurShader = open_shader("/Users/sky/Documents/c/open_gl/shader/blur.vert", "/Users/sky/Documents/c/open_gl/shader/blur.frag",NULL);
-    uint32_t mergeShader = open_shader("/Users/sky/Documents/c/open_gl/shader/bloom_final.vert", "/Users/sky/Documents/c/open_gl/shader/bloom_final.frag",NULL);
-    glUseProgram(baseShader);
+    uint32_t shader = open_shader("/Users/sky/Documents/c/open_gl/shader/pbr.vert", "/Users/sky/Documents/c/open_gl/shader/pbr.frag",NULL);
+    glUseProgram(shader);
     vec4* proj = GLM_MAT4_IDENTITY;
-    glm_perspective(GLM_PI_4,1,1,40,proj);
-    uniform_mat4(baseShader, "projection", proj);
-    uniform_v3(baseShader, "lights[0].Position", 0.0f, 0.5f,  1.5f);
-    uniform_v3(baseShader, "lights[0].Color", 5.0f,   5.0f,  5.0f);
-    uniform_v3(baseShader, "lights[1].Position", -4.0f, 0.5f, -3.0f);
-    uniform_v3(baseShader, "lights[1].Color", 10.0f,  0.0f,  0.0f);
-    uniform_v3(baseShader, "lights[2].Position",  3.0f, 0.5f,  1.0f);
-    uniform_v3(baseShader, "lights[2].Color", 0.0f,   0.0f,  15.0f);
-    uniform_v3(baseShader, "lights[3].Position", -0.8f,  2.4f, -1.0f);
-    uniform_v3(baseShader, "lights[3].Color", 0.0f,   5.0f,  0.0f);
-    glUseProgram(lightShader);
-    proj = GLM_MAT4_IDENTITY;
-    glm_perspective(GLM_PI_4,1,1,25,proj);
-    uniform_mat4(lightShader, "projection", proj);
-    glUseProgram(mergeShader);
-    uniform_i1(mergeShader, "scene", 0);
-    uniform_i1(mergeShader, "bloomBlur", 1);
+    glm_perspective(GLM_PI_4,16.0f/9.0f,1,40,proj);
+    uniform_mat4(shader, "projection", proj);
+    uniform_f1(shader,"ao",1);
+    uniform_v3(shader, "lightPositions[0]", -10.0f,  10.0f, 10.0f);
+    uniform_v3(shader, "lightPositions[1]", 10.0f,  10.0f, 10.0f);
+    uniform_v3(shader, "lightPositions[2]", -10.0f, -10.0f, 10.0f);
+    uniform_v3(shader, "lightPositions[3]", 10.0f, -10.0f, 10.0f);
+    uniform_v3(shader, "lightColors[0]", 300.0f, 300.0f, 300.0f);
+    uniform_v3(shader, "lightColors[1]", 300.0f, 300.0f, 300.0f);
+    uniform_v3(shader, "lightColors[2]", 300.0f, 300.0f, 300.0f);
+    uniform_v3(shader, "lightColors[3]", 300.0f, 300.0f, 300.0f);
 
-    uint32_t cube = create_texture("/Users/sky/Documents/c/open_gl/res/container2.png",GL_TEXTURE0);
+    obj_t *obj=load_obj("/Users/sky/Documents/c/open_gl/res/reisen.obj");
 
-    frame_buff_t *hdr_frame= create_frame_buff2(1280*2,720*2); // 注意mac屏幕缩放
-    frame_buff_t *blur_frame=create_frame_buff(1280*2,720*2);
-
-    uint32_t cube_vao=create_vao();
-    glBufferData(GL_ARRAY_BUFFER,sizeof(cube_vnt),cube_vnt,GL_STATIC_DRAW);
-    vertex_attr(0,3,8,0);
-    vertex_attr(1,3,8,3);
-    vertex_attr(2,2,8,6);
-    uint32_t quad_vao=create_vao();
-    glBufferData(GL_ARRAY_BUFFER,sizeof(quad_vt),quad_vt,GL_STATIC_DRAW);
-    vertex_attr(0,3,5,0);
-    vertex_attr(1,2,5,3);
+    uint32_t texture= create_texture("/Users/sky/Documents/c/open_gl/res/reisen.png",GL_TEXTURE0);
 
     glEnable(GL_DEPTH_TEST); // 正常渲染启动深度测试
     vec4 *view=GLM_MAT4_IDENTITY;
@@ -70,98 +50,29 @@ int main(){
         camera_update(&camera,window);
         camera_view(&camera,view);
 
-        // 绘制到普通场景与 hdr buff 中
-        glBindFramebuffer(GL_FRAMEBUFFER, hdr_frame->frame_buff);
         glClearColor(0.1f,0.1f,0.1f,1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        // 先绘制cube
-        glUseProgram(baseShader);
-        uniform_mat4(baseShader,"view",view);
-        uniform_v3(baseShader,"viewPos",camera.pos[0],camera.pos[1],camera.pos[2]);
-        set_texture(GL_TEXTURE0,cube);
-        glBindVertexArray(cube_vao);
-        vec4* model=GLM_MAT4_IDENTITY;
-        translate(model,0,-1,0);
-        scale(model,10,10,10);
-        uniform_mat4(baseShader,"model",model);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,0,1.5f,0);
-        scale(model,0.5f,0.5f,0.5f);
-        uniform_mat4(baseShader,"model",model);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,2,0,1);
-        scale(model,0.5f,0.5f,0.5f);
-        uniform_mat4(baseShader,"model",model);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,-1,-1,2);
-        uniform_mat4(baseShader,"model",model);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,0,2.7f,4);
-        scale(model,1.25f,1.25f,1.25f);
-        uniform_mat4(baseShader,"model",model);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,-2,1,-3);
-        uniform_mat4(baseShader,"model",model);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,-3,0,0);
-        scale(model,0.5f,0.5f,0.5f);
-        uniform_mat4(baseShader,"model",model);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        // 再绘制灯光
-        glUseProgram(lightShader);
-        uniform_mat4(lightShader,"view",view);
-        glBindVertexArray(cube_vao);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,0,0.5f,1.5f);
-        uniform_mat4(lightShader,"model",model);
-        uniform_v3(lightShader,"lightColor",5,5,5);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,-4,0.5f,-3);
-        uniform_mat4(lightShader,"model",model);
-        uniform_v3(lightShader,"lightColor",10,0,0);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,3,0.5f,1);
-        uniform_mat4(lightShader,"model",model);
-        uniform_v3(lightShader,"lightColor",0,0,15);
-        glDrawArrays(GL_TRIANGLES,0,36);
-        model=GLM_MAT4_IDENTITY;
-        translate(model,-0.8f,2.4f,-1);
-        uniform_mat4(lightShader,"model",model);
-        uniform_v3(lightShader,"lightColor",0,5,0);
-        glDrawArrays(GL_TRIANGLES,0,36);
-
-        // 高斯模糊
-        glBindFramebuffer(GL_FRAMEBUFFER, blur_frame->frame_buff);
-        glClearColor(0.1f,0.1f,0.1f,1);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(blurShader);
-        set_texture(GL_TEXTURE0,hdr_frame->text_buff2);
-        glBindVertexArray(quad_vao);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-        // 最终合并写入
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        glClearColor(0.1f,0.1f,0.1f,1);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glUseProgram(mergeShader);
-        set_texture(GL_TEXTURE0,hdr_frame->text_buff);
-        set_texture(GL_TEXTURE1,blur_frame->text_buff);
-        glBindVertexArray(quad_vao);
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        glUseProgram(shader);
+        set_texture(GL_TEXTURE0,texture);
+        uniform_mat4(shader,"view",view);
+        uniform_v3(shader,"camPos",camera.pos[0],camera.pos[1],camera.pos[2]);
+        for (int row = 0; row < 4; ++row){
+            uniform_f1(shader,"metallic",(float)row/4.0f);
+            for (int col = 0; col < 4; ++col){
+                uniform_f1(shader,"roughness",(float)col/4.0f);
+                vec4 *model = GLM_MAT4_IDENTITY;
+                translate(model,(float)(col-2)*4,(float)(row-2)*4,0);
+                uniform_mat4(shader,"model",model);
+                glBindVertexArray(obj->vao);
+                glDrawArrays(GL_TRIANGLES,0,obj->point_count);
+            }
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    close_vao(cube_vao);
-    close_shader(baseShader);
+    close_obj(obj);
+    close_shader(shader);
     glfwTerminate();
     return 0;
 }
